701b0bf9a2afe6f02eaf9c6c2a3bcad749343a43
Tony Vashevko <avashevko@gmail.com>
2014-10-03 10:31:01 -0700

    symlinks update

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -2,4 +2,6 @@ ln -sf ~/.dotfiles/.tmux.conf ~
 ln -sf ~/.dotfiles/.vimrc ~
 ln -sf ~/.dotfiles/.Rprofile ~
 ln -sf ~/.dotfiles/.zshrc ~ 
+ln -sf ~/.dotfiles/panopy.templates ~/.pandoc
+ln -sf ~/.dotfiles/tex.gpp ~/.pandoc
 
