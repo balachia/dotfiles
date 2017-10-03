35ce36d2aed5c27b6e2446259a5bf8b56c64a795
Tony Vashevko <avashevko@gmail.com>
2017-10-03 10:37:32 +0800

    another emacs attempt

--- init.el
+++ init.el
@@ -10,4 +10,26 @@
 (require 'evil)
 (evil-mode 1)
 
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;;;;Org mode configuration
+;; Enable Org mode
+(require 'org)
+;; Make Org mode work with files ending in .org
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; The above is the default in recent emacsen
+
+;; themes
+(defun theme-toggle ()
+  (interactive)
+  (let* ((ring '(material material-light))
+         (current (if (get 'theme-toggle 'state) (get 'theme-toggle 'state) -1))
+         (next-idx (% (+ current 1) (length ring)))
+         (next (nth next-idx ring))
+         )
+  (put 'theme-toggle 'state next-idx)
+  (load-theme next t)))
+
+(theme-toggle)
 
