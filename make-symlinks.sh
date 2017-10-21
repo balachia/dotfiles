87f7510f53ede383fed4d1a7039b859790d6b0dd
Tony Vashevko <avashevko@gmail.com>
2017-10-21 14:43:15 +0800

    error

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,6 +1,7 @@
 mkdir -p ~/.config/nvim
 mkdir -p ~/.emacs.d
-mkdir -p ~/.local/{bin,src,R/site-library}
+mkdir -p ~/.local/{bin,src,R}
+mkdir -p ~/.local/R/site-library
 
 ln -sf ~/.dotfiles/.tmux.conf ~
 ln -sf ~/.dotfiles/.vimrc ~
