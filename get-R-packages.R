5387ec4319e77717913107490476f50598eaf00b
Tony Vashevko <avashevko@gmail.com>
2017-10-21 14:25:07 +0800

    r packages

---
+++ get-R-packages.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+
+install.packages('devtools')
+devtools::install_github("hadley/devtools")
+
+devtools::install_github("jalvesaq/colorout")
+
+install.packages('purrr')
+install.packages('data.table')
+install.packages('ggplot2')
+
