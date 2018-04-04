0059f00456a85a6dba8ff0e4816614d9765b6ba3
Tony Vashevko <avashevko@gmail.com>
2018-04-04 21:28:20 +0800

    kitty and related tmux fixes

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/kitty
 mkdir -p ~/.emacs.d
 mkdir -p ~/.local/{bin,src,R\/site-library}
 
@@ -9,6 +10,7 @@ ln -sf ~/.dotfiles/.Rprofile ~
 #ln -sf ~/.dotfiles/.zshrc.oh-my-zsh ~/.zshrc 
 ln -sf ~/.dotfiles/.zshrc.prezto ~/.zshrc 
 ln -sf ~/.dotfiles/.inputrc ~ 
+ln -sf ~/.dotfiles/kitty.conf ~/.config/kitty
 ln -sf ~/.dotfiles/panopy.templates ~/.pandoc
 ln -sf ~/.dotfiles/tex.gpp ~/.pandoc
 ln -sf ~/.dotfiles/init.el ~/.emacs.d/init.el
